b9f9945956d011748937f2feb75b47f4e5827e8d
Jaelynn Shurley <jaelynnshurley@live.com>
2026-02-19 16:08:00 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -145,17 +145,23 @@ function displayData(data){
 	// page element to display 7 day forecast
 	let forecast = document.getElementById("forecast");
 
+	// if the HTML elements aren't found, stop before crashing
+	if(!current || !forecast){
+		console.error('Missing #current or #forecast element in the HTML.');
+		return;
+	}
+
 	// if we got an error in response, display that to the page/user
 	if(data.error){
-		current.innerHTML = `<p>There was an error: ${"TO DO - ADD ERROR MESSAGE"}. Please try again later.</p>`;
+		current.innerHTML = `<p>There was an error: ${data.reason}. Please try again later.</p>`;
 	}else{
 		// if there were no errors, let's add the weather to the page
 		let today = new Date();
 		//the current weather
-		let currentHTML = `<h3>${"TO DO - ADD LOCATION NAME"}</h3>
-							<p>${"TO DO - Add the day of the week (full name)"}, ${"TO DO - add the month (full name)"} ${"TO DO - add the day of the month (numerical)"}</p>
-							<img src="${"TO DO - Add the icon from the conditions object above based on the weather code for this day"}" alt="${"TO DO - add the description of the weather on this day from the conditions object above based on the weather code"}">
-							<p><b>Current Weather: </b>${"TO DO - add the temperature without any decimal places"}&deg; and ${"TO DO - add the description of the weather on this day from the conditions object above based on the weather code"}</p>
+		let currentHTML = `<h3>${data.timezone}</h3>
+							<p>${today.toLocaleString("en-us", {weekday: "long"})}, ${today.toLocaleString("en-us", {month: "long"})} ${today.getDate()}</p>
+							<img src="${conditions[data.current.weather_code].path}" alt="${conditions[data.current.weather_code].desc}">
+							<p><b>Current Weather: </b>${Math.round(data.current.temperature_2m)}&deg; and ${conditions[data.current.weather_code].desc}</p>
 							`;
 		// add the current weather information to the page
 		current.innerHTML = currentHTML;
@@ -166,15 +172,15 @@ function displayData(data){
 		//the upcoming forecast
 		for(let i = 0; i < 8; i++){
 			// generate a date object from the given date for this day in the forecast
-			let date; // TO DO - create a new date object using the information returned by the API
+			let date = new Date(data.daily.time[i]);
 
 			// add the weather for each date to the page with the information listed in comments above
 			forecastHTML += `<section class="day">
-								<h3><span>${"TO DO - Add the day of the week (full name)"}</span> ${"TO DO - add the month (full name)"} ${"TO DO - add the day of the month (numerical)"}</h3>
-								<img src="${"TO DO - Add the icon from the conditions object above based on the weather code for this day"}" alt="${"TO DO - add the description of the weather on this day from the conditions object above based on the weather code"}">
-								<p><b>High: </b>${"TO DO - add the high temperature for this day without any decimal places"}</p>
-								<p><b>Low: </b>${"TO DO - add the high temperature for this day without any decimal places"}</p>
-								<p>${"TO DO - add the description of the weather on this day from the conditions object above based on the weather code"}</p>
+								<h3><span>${date.toLocaleString("en-us", {weekday: "long"})}</span> ${date.toLocaleString("en-us", {month: "long"})} ${date.getDate()}</h3>
+								<img src="${conditions[data.daily.weather_code[i]].path}" alt="${conditions[data.daily.weather_code[i]].desc}">
+								<p><b>High: </b>${Math.round(data.daily.temperature_2m_max[i])}&deg;</p>
+								<p><b>Low: </b>${Math.round(data.daily.temperature_2m_min[i])}&deg;</p>
+								<p>${conditions[data.daily.weather_code[i]].desc}</p>
 							</section>`;
 		}
 	// add the complete upcoming forecast to the page
@@ -200,11 +206,20 @@ async function getWeather(location){
 			- also getting the current temperature and weather code
 			- temperature should be returned in Fahrenheit
 		*/
-	let endpoint = `https://api.open-meteo.com/v1/`; // TO DO - complete the endpoint to return the data we need
+	let endpoint = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,weather_code&forecast_days=8&current=temperature_2m,weather_code&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
 
 
 	// fetch call to API
-	// TO DO
+	await fetch(endpoint)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			displayData(data);
+		})
+		.catch(err => {
+			console.error(err.message);
+		});
+
 }
 
 //on page load, get geolocation
